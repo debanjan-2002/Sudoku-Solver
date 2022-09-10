1718a58cd7f07b6feb3851123315d21a4b719cac
Debanjan Poddar <debanjan2602@gmail.com>
2022-09-10 23:35:01 +0530

    Added logic for Alert Box

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,4 @@
+const alertBox = document.querySelector('.alert-box');
 const boardContainer = document.querySelector('#board-container');
 const optionsContainer = document.querySelector('#options-container');
 const button = document.querySelector('button');
@@ -5,19 +6,7 @@ const startButton = document.querySelector('.start');
 const stopButton = document.querySelector('.stop');
 let selectedOption = null;
 
-// const exampleBoard = [
-//     ["7","6","2",".",".","9",".","1","."],
-//     ["3",".","4",".",".",".",".",".","2"],
-//     ["8",".",".",".","5","6","4",".","7"],
-//     [".","9","7","1",".",".",".",".","."],
-//     [".",".","6",".",".","8","7",".","3"],
-//     ["5","8",".",".","6",".","2",".","1"],
-//     [".","7",".","9",".",".","3",".","."],
-//     ["9",".","1","4","2",".",".","8","6"],
-//     ["2","4","5",".",".","3",".",".","."]
-// ];
-
-const exampleBoard = [
+const sudokuBoard = [
     [".",".",".",".",".",".",".",".","."],
     [".",".",".",".",".",".",".",".","."],
     [".",".",".",".",".",".",".",".","."],
@@ -51,16 +40,16 @@ function selectBlock(block, row, col) {
                 return;
             }
             block.innerText = "";
-            block.classList.remove("initial-block-color");
-            exampleBoard[row][col] = ".";
+            block.classList.remove("selected-color");
+            sudokuBoard[row][col] = ".";
         }
         else if(block.innerText !== "") {
             return;
         }
         else {
             block.innerText = selectedOption.innerText;
-            block.classList.add("initial-block-color");
-            exampleBoard[row][col] = block.innerText;
+            block.classList.add("selected-color");
+            sudokuBoard[row][col] = block.innerText;
         }
     }
 }
@@ -68,7 +57,7 @@ function selectBlock(block, row, col) {
 function buildBoard() {
     for(let i = 0; i <= 9; i++) {
         const num = document.createElement("div");
-        num.classList.add('options', 'flex', 'justify-center', 'align-center', 'font-wt-bold');
+        num.classList.add('options', 'flex', 'justify-center', 'align-center', 'font-wt-bold', 'br-radius');
         if(i == 0) {
             num.innerText = "C";
         }
@@ -101,24 +90,31 @@ function buildBoard() {
 
 const valid = async (row, col, c) => {
     for(let i = 0; i < 9; i++) {
-        if(parseInt(exampleBoard[i][col]) === c) return false;
-        if(parseInt(exampleBoard[row][i]) === c) return false;
+        if(parseInt(sudokuBoard[i][col]) === c) return false;
+        if(parseInt(sudokuBoard[row][i]) === c) return false;
         let rw = (3 * Math.floor(row / 3)) + Math.floor(i / 3);
         let cl = (3 * Math.floor(col / 3)) + Math.floor(i % 3);
-        if(parseInt(exampleBoard[rw][cl]) === c) return false;
+        if(parseInt(sudokuBoard[rw][cl]) === c) return false;
     }
     return true;
 }
 
 const start = async () => {
-    const isPossible = validBoard(exampleBoard);
+    const isPossible = validBoard(sudokuBoard);
     if(isPossible) {
-        optionsContainer.style.display = 'none';
+        alertBox.classList.add('display-none');
+        optionsContainer.classList.add('display-none');
+        
         const result = await solve();
-        console.log(result);
+
+        alertBox.classList.remove('display-none', 'alert-failure');
+        alertBox.classList.add('alert-success');
+        alertBox.innerText = "Solved";
     }
     else {
-        console.log("false");
+        alertBox.classList.remove('display-none', 'alert-success');
+        alertBox.classList.add('alert-failure');
+        alertBox.innerText = "Unsolvable";
     }
 }
 
@@ -129,18 +125,18 @@ const stop = () => {
 const solve = async () => {
     for(let i = 0; i < 9; i++) {
         for(let j = 0; j < 9; j++) {
-            if(exampleBoard[i][j] === ".") {
+            if(sudokuBoard[i][j] === ".") {
                 for(let c = 1; c <= 9; c++) {
                     if(await valid(i, j, c)) {
                         await delay();
-                        exampleBoard[i][j] = c.toString();
+                        sudokuBoard[i][j] = c.toString();
                         document.getElementById(`${i}-${j}`).innerText = c;
                         
                         const res = await solve();
                         if(res) return true;
                         
                         await delay();
-                        exampleBoard[i][j] = ".";
+                        sudokuBoard[i][j] = ".";
                         document.getElementById(`${i}-${j}`).innerText = "";
                     }
                 }
